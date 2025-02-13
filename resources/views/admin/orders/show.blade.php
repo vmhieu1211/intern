54b824d50f60c3a7b08648c956416f056029e405
vmhieu1211 <cavaldos1211@gmail.com>
2025-02-13 13:33:05 +0700

    update

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -13,7 +13,7 @@
 
         <div class="card">
             <div class="card-header">{{ $order->order_number }} <strong class="ml-5">Tổng tiền:
-                    $ {{ $order->billing_total }} </strong></div>
+                    {{ $order->billing_total }}đ </strong></div>
             <div class="card-body">
                 <h4>Sản phẩm đã đặt hàng</h4>
                 <table class="table table-bordered table-responsive table-dark">
@@ -28,7 +28,7 @@
                             <tr>
                                 <td>{{ $p->code }}</td>
                                 <td>{{ $p->name }}</td>
-                                <td>${{ $p->price }}</td>
+                                <td>{{ $p->price }}đ</td>
                                 <td>{{ $p->pivot->quantity }}</td>
                             </tr>
                         @endforeach
