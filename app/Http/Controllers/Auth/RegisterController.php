8e378b6c3bbfaddb86601ba5572c38131203b757
vmhieu1211 <cavaldos1211@gmail.com>
2025-03-01 15:56:40 +0700

    update register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Validator;
+ use Illuminate\Support\Facades\Validator;  
+
 use Illuminate\Foundation\Auth\RegistersUsers;
 
 class RegisterController extends Controller
