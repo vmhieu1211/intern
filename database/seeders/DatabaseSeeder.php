0b93fee72d2873b845c48fc3cc2a21e0bfbe16ea
vmhieu1211 <cavaldos1211@gmail.com>
2025-03-12 11:30:23 +0700

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,6 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         $this->call(SystemSettingSeeder::class);
-
+        $this->call(UserSeeder::class);
     }
 }
